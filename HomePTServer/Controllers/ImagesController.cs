c512d7e372040b528e32fbf1c83b5ba4e71405ae
Nathan Ziebart <nathan.ziebart@gmail.com>
2013-04-23 22:36:05 -0400

    changed image handling for messages

---
+++ HomePTServer/Controllers/ImagesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomePTServer.Models;
+using System.Web.Script.Serialization;
+
+namespace HomePTServer.Controllers
+{
+    public class ImagesController : Controller
+    {
+        //
+        // GET: /Images/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public class ImageArgs
+        {
+            public string name { get; set; }
+        }
+
+        [HttpPost]
+        public String GetFullImage(ImageArgs args) {
+            Dictionary<string, object> results = new Dictionary<string, object>();
+            try {
+                results["image"] = Convert.ToBase64String(GetImageData(args.name, false));
+            } catch (Exception e) {
+                results["error"] = "Invalid image name.";
+            }
+            return new JavaScriptSerializer().Serialize(results);
+        }
+
+        [HttpPost]
+        public String GetThumbImage(ImageArgs args) {
+            Dictionary<string, object> results = new Dictionary<string, object>();
+            try {
+                results["image"] = Convert.ToBase64String(GetImageData(args.name, true));
+            } catch (Exception e) {
+                results["error"] = "Invalid image name.";
+            }
+            return new JavaScriptSerializer().Serialize(results);
+        }
+
+        public static byte[] GetImageData(string imageName, bool thumbSize) {
+            return System.IO.File.ReadAllBytes(PTDatabase.PathForImageNamed(imageName, thumbSize));
+        }
+
+    }
+}
